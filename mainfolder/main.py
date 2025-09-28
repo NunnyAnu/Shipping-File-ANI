5cdfac1f0b16548c24de609fb16577f2d6b472d1
NunnyAnu <115398044+NunnyAnu@users.noreply.github.com>
2025-09-28 17:22:18 +0700

    .

--- mainfolder/main.py
+++ mainfolder/main.py
@@ -22,7 +22,8 @@ class ANIProcessor:
         branch_org_code = org_code                                      
         match = re.search(r'_FS[\s_]*?(\d{2})-(\d{2})', filename_no_ext) # Extract year and month
         if not match:
-            raise ValueError(f"Filename '{filename}' does not match expected pattern")
+            print(f"Filename '{filename}' does not match expected pattern") # if the filename is in the wrong format, skip this file
+            return
         year_suffix, month = match.groups()
         year = "20" + year_suffix                                        # millennium 20xx
         docu_date = f"01/{month}/{year}"
@@ -47,21 +48,24 @@ class ANIProcessor:
                 header_row = i
                 break
         if header_row is None:
-            print(f"Skipping {file} — 'Account No.' not found")
+            print(f"Skipping {file} — 'Account No.' not found")             # if header row('Account No.' ) is not found, skip this file
             return
         df.columns = df.iloc[header_row].astype(str).str.strip()
         data = df.iloc[header_row+1:].reset_index(drop=True)
 
         required_cols = ["Account No.", "Account Description", "THB"]
-        if all(col in data.columns for col in required_cols):
-            data = data.rename(columns={            # Rename columns
+
+        missing_cols = [col for col in required_cols if col not in data.columns]
+        if not missing_cols:   
+            data = data.rename(columns={
                 "Account No.": "AccCode",
                 "Account Description": "AccName",
                 "THB": "AcccumMonthAmnt"
             })
         else:
-            print(f"Skipping {file} — required columns not found")
-            return  # skip this file
+            print(f"Skipping {file} — missing columns: {', '.join(missing_cols)}")   # if required columns are not found, skip this file.
+            return 
+
 
         data = data[["AccCode", "AccName", "AcccumMonthAmnt"]]
         data = data[data["AccCode"].notna() & (data["AccCode"].astype(str).str.strip() != "")]      # Remove rows where AccCode is empty
@@ -86,12 +90,18 @@ class ANIProcessor:
     def run(self):
         for file in os.listdir(self.input_folder):
             print(file)
-            if file.endswith(".xlsx"):
-                file_path = os.path.join(self.input_folder, file)
-                company_info = self.parse_filename(file)
-                # print(company_info)
-                self.process_data(file_path, company_info, temp_csv)
-                self.file_info = company_info                                                                # save last file info for naming Excel
+            if not file.endswith(".xlsx"):                                   # if the file is not an .xlsx, skip this file.
+                continue
+
+            file_path = os.path.join(self.input_folder, file)
+            company_info = self.parse_filename(file)
+            # print(company_info)
+
+            if company_info is None:                                        # if the filename is in the wrong format, skip this file.
+               continue
+
+            self.process_data(file_path, company_info, temp_csv)
+            self.file_info = company_info                                                                # save last file info for naming Excel
         if self.file_info:
             self.convert_csv_to_xlsx(temp_csv, output_folder, self.file_info["DateYear"], self.file_info["DateMonth"])
 
